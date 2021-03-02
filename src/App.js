cce05f3f893e48040821351b8a63c32a79430542
Saad Saif <saadgfx97@gmail.com>
2021-03-03 00:13:18 +0500

    added my name

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
       </header>
+      <h1>Created by saad saif</h1>
     </div>
   )
 }
